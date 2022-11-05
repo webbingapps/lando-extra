558e82ac7d64a4856096466e65398a55d2fde51f
texeltexel2009 <texeltexel2009@gmail.com>
2022-11-05 18:25:40 +0100

    Add WordPress cronjobs

--- scripts/wordpress/setup-wordpress-utils.sh
+++ scripts/wordpress/setup-wordpress-utils.sh
@@ -5,3 +5,6 @@ cd /tmp/ || return
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
+
+# Add WordPress cron job
+crontab /lando-extra/configs/wordpress/cron-wordpress.txt
