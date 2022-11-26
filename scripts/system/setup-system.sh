6df8d4026b89e87311361965b4330aa9b832a1db
texeltexel2009 <texeltexel2009@gmail.com>
2022-11-26 18:51:41 +0100

    Add procps for ps command, lsb-release and net-tools for netstat commands

--- scripts/system/setup-system.sh
+++ scripts/system/setup-system.sh
@@ -3,7 +3,7 @@
 # System
 apt update -y
 apt upgrade -y
-apt install vim neofetch net-tools openssh-server wget lsof iptables cron iputils-ping -y
+apt install procps lsb-release vim neofetch openssh-server wget net-tools lsof iptables cron iputils-ping -y
 apt clean -y && apt autoclean -y && apt autoremove -y && rm -rf /var/lib/apt/lists/* && rm -rf /var/lib/cache/* && rm -rf /var/lib/log/* && rm -rf /tmp/*
 
 # Miscelanea
