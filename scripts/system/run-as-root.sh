96e279a3e84b1a83ce4e16f53f339a8a82a75197
texeltexel2009 <texeltexel2009@gmail.com>
2022-12-02 20:37:23 +0400

    Optimize start as root tasks

--- scripts/system/run-as-root.sh
+++ scripts/system/run-as-root.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Start services
-service ssh start
-service cron start
+if [ -x "$(command -v ssh)" ]; then
+  service ssh start
+fi
+if [ -x "$(command -v cron)" ]; then
+  service cron start
+fi
 if [ -x "$(command -v redis-server)" ]; then
   redis-server /lando-extra/configs/redis/redis.conf --daemonize yes
 fi
