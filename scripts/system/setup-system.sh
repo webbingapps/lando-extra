f540071d76005fae65d9c98372cdbd9e9de91732
texeltexel2009 <texeltexel2009@gmail.com>
2022-12-02 20:38:09 +0400

    Optimize system update & upgrade

--- scripts/system/setup-system.sh
+++ scripts/system/setup-system.sh
@@ -4,7 +4,6 @@
 apt update -y
 apt upgrade -y
 apt install procps lsb-release vim neofetch openssh-server wget net-tools lsof iptables cron iputils-ping -y
-apt clean -y && apt autoclean -y && apt autoremove -y && rm -rf /var/lib/apt/lists/* && rm -rf /var/lib/cache/* && rm -rf /var/lib/log/* && rm -rf /tmp/*
 
 # Miscelanea
 
