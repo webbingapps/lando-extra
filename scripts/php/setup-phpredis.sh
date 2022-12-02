321bc00799b9f2cbdd2050a56584f92b3e027dd1
texeltexel2009 <texeltexel2009@gmail.com>
2022-12-02 20:38:41 +0400

    Add phpredis extension installation and setup

--- scripts/php/setup-phpredis.sh
+++ scripts/php/setup-phpredis.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Init
+cd /tmp/ || exit
 readonly LANDO_EXTRA_PATH=/lando-extra
 readonly APP_PATH=/app
 
 # Install
 echo "Installing PHPRedis"
-pickle -n install redis
+# Doesn't seem to be working though
+yes | pickle install redis
+
+#apt update -y
+#apt install libzstd-dev liblz4-dev -y
+#pecl install -D 'enable-redis-igbinary="yes" enable-redis-lzf="yes" enable-redis-lz4="yes" enable-redis-zstd="yes" with-liblz4="yes"' redis
